f156444d8ed040d53d66851ed3e0ea80527a37d6
kbriske <70216437+kbriske@users.noreply.github.com>
2020-08-31 20:34:47 -0400

    added more shapes

--- Assignments/Personal Composition/js/app.js
+++ Assignments/Personal Composition/js/app.js
@@ -1,6 +1,11 @@
 function setup(){
     createCanvas(800,600);
     background(0);
-    circle(400,300,50);
+    "circle(400,300,50); maybe keep"
+    circle(400,550,150);
+    circle(400,470,120);
+    circle(400,400,80);
+    rect(357,350,85,30);
+    rect(377,325,40,40);
 
 }
