a867ee4b8fe69d582b2e8ba3d4b48b7a4f189adc
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-01 11:47:04 -0400

    added comments to the personal composition file, and cleared errors for the practice file by adding a js folder

--- Assignments/Practice/app.js
+++
@@ -1,4 +0,0 @@
-let a = 6;
-let b = 6;
-let c = sqrt(a + b);
-document.write(c);
---
+++ Assignments/Practice/js/app.js
@@ -0,0 +1,9 @@
+//a equals a number
+var a = 2;
+//b equals a number
+var b = 2;
+//c equals the square root of a + b
+var c = Math.sqrt(a*a + b*b);
+console.log(c);
+//*allegedly writes on page, but wont work right now..
+document.write(c);
