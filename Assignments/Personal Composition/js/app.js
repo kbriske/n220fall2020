769c0ca6d980f41044c5bbb6e06c5e229949a246
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-03 10:51:55 -0400

    added another snowman and added the base of the house

--- Assignments/Personal Composition/js/app.js
+++ Assignments/Personal Composition/js/app.js
@@ -1,12 +1,21 @@
 function setup(){
     createCanvas(800,600);
     background(0);
+    rect(200,300,500,300);
+    trianle()
     "circle(400,300,50); maybe keep"
     circle(400,550,150);
     circle(400,470,120);
     circle(400,400,80);
     rect(357,350,85,30);
     rect(377,325,40,40);
-    "rect(200,200,500,500);"
+   
+    triangle(400,400,310,300,300,300);
+
+    circle(200,550,150);
+    circle(200,470,120);
+    circle(200,400,80);
+    rect(157,350,85,30);
+    rect(177,325,40,40);
 
 }
