8eefdfb2e4727850ed09ded894d0c32318ee0433
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-07 12:03:10 -0400

    created a new assignment, and started work on the counter exercise

---
+++ Assignments/Animation and Conditional Operators Exercises/js/app.js
@@ -0,0 +1,17 @@
+//counter
+
+let counter = 0;
+function setup(){
+    createCanvas(400,300);
+}
+function draw(){
+    background(62,237,108)
+
+    fill(19,71,26);
+
+    if(counter > -1){
+        console.log(counter)
+        
+    }
+}
+/////
