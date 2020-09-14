45e8f743636a96fc1297bcae9451aa6a2e633bbc
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-14 10:47:18 -0400

    got a circle on the screen, trying to figure out how to make duplicated until a condition is met

--- Assignments/Loops Exercises/FizzBuzz/js/app.js
+++ Assignments/Loops Exercises/FizzBuzz/js/app.js
@@ -1,6 +1,6 @@
 count = 0;
-x = 200
-y = 150
+x = 50
+y = 50
 
 /*function setup() {
     background(0);
@@ -9,12 +9,13 @@ y = 150
 function draw() {
     createCanvas(800,600);
     background(211,241,255);
+    circle(x,y,99);
+    x = x + 2;
 }
 
 
 while(count < 25) {
     count = count+1;
     console.log(count);
-    /*x = x + 5;
-    circle(x,y,30);*/
+    //circle(x,y,30);
 }
