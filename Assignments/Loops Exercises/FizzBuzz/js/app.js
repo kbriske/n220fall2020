6941bf99f283491fdb77c99917d6da5b9667aa74
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-13 22:48:53 -0400

    made some more progress, wrote some code, the usual

---
+++ Assignments/Loops Exercises/FizzBuzz/js/app.js
@@ -0,0 +1,20 @@
+count = 0;
+x = 200
+y = 150
+
+/*function setup() {
+    background(0);
+}*/
+
+function draw() {
+    createCanvas(800,600);
+    background(211,241,255);
+}
+
+
+while(count < 25) {
+    count = count+1;
+    console.log(count);
+    /*x = x + 5;
+    circle(x,y,30);*/
+}
