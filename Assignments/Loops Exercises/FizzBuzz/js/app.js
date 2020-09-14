e8e3630f43717866ab5d38ece3647aa2f55be29d
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-14 16:59:38 -0400

    made circles draw in a line, then got squares to show up as well, all with correct colors

--- Assignments/Loops Exercises/FizzBuzz/js/app.js
+++ Assignments/Loops Exercises/FizzBuzz/js/app.js
@@ -1,21 +1,31 @@
 count = 0;
 x = 50
 y = 50
+y2 = 41
 
-/*function setup() {
-    background(0);
-}*/
+//canvas creation
+function setup() {
+    createCanvas(800,600);
+}
 
 function draw() {
-    createCanvas(800,600);
-    background(211,241,255);
-    circle(x,y,99);
-    x = x + 2;
+    //background(211,241,255);
+    while(count < 25) {
+        count = count+1;
+        console.log(count);
+        circle(count *20,y,18);
+        if(count%3 == 0) {
+            fill(128, 52, 235);
+            circle(count *20,y,18);
+            noFill();
+        }
+        if(count%5 == 0) {
+            fill(52, 235, 73);
+            square(count*18,y2,19); //count*20,y,18
+            noFill();
+        }
+    }
 }
 
 
-while(count < 25) {
-    count = count+1;
-    console.log(count);
-    //circle(x,y,30);
-}
+
