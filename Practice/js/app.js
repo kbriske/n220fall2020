2ef4204225e763d0c2fd103e598581210ac609f4
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-15 11:20:06 -0400

    played around with the in-class pratice in the practice file

--- Practice/js/app.js
+++ Practice/js/app.js
@@ -1,15 +1,21 @@
+let timer = 5;
 function setup() {
     createCanvas(400,400);
+    background(0);
+    frameCount = (0);
+    frameRate = (30);
 }
 
 
 function draw() {
-    background(220);
-    console.log(mouseX,"x");
-    console.log(mouseY,"y");
+    background(0);
+    if (frameCount % 60 == 0 && timer > 0) {
+        fill(66, 25, 250);
+        circle(mouseX,mouseY,30); 
+    }
+    else {
+        fill(255, 25, 33);
+        circle(mouseX,mouseY,30);
+    }
+}
 
-circle(mouseX, mouseY, 40);
-}
-if(mouseX, mouseY > 200){
-    fill("yellow")
-}
