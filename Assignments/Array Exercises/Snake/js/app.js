81efc967df757db0111bc9a88090b4dbcaf179b2
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-22 17:10:33 -0400

    created the snake array exercise

---
+++ Assignments/Array Exercises/Snake/js/app.js
@@ -0,0 +1,11 @@
+let posX = [0,0,0,0,0,0,0,0,0,0];
+let posY = [0,0,0,0,0,0,0,0,0,0];
+
+function setup() {
+    createCanvas(700,700);
+    background(255, 218, 117);
+}
+
+function draw() {
+    
+}
