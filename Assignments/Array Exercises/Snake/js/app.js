571ac28ad10d10efa457c0c11e121ccd71b32a7f
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-23 11:04:53 -0400

    attempted more with the snake exercise

--- Assignments/Array Exercises/Snake/js/app.js
+++ Assignments/Array Exercises/Snake/js/app.js
@@ -1,11 +1,32 @@
+/*
 let posX = [0,0,0,0,0,0,0,0,0,0];
 let posY = [0,0,0,0,0,0,0,0,0,0];
 
 function setup() {
     createCanvas(700,700);
-    background(255, 218, 117);
 }
 
 function draw() {
-    
+    background(255, 218, 117);
+    circle(mouseX,mouseY,30);
+}
+*/
+let positions = [0,0,0,0]; //the more you add the more trail the circle will have
+
+function setup() {
+    createCanvas(400,300);
+
+
+}
+
+function draw() {
+    background(0);
+    console.log(positions);
+
+    positions.push(mouseX, mouseY);
+    positions.shift();
+
+    for(var i = 0; i < positions.length; i++) {
+        circle(positions[i], 150,20);
+    }
 }
