5396665f2bae4aaafa6f89b51bbb830251cebf4d
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-30 12:13:12 -0400

    created personal composition in function practice

---
+++ Assignments/Functions Exercises/Personal Composition/js/app.js
@@ -0,0 +1,17 @@
+let moonX = 0;
+let moonY = 200;
+
+function setup() {
+    createCanvas(700,400);
+    background(0);
+}
+
+function draw() {
+    background(0);
+    moonX++;
+    moonY++;
+}
+
+function drawMoon(moonColor,crater,heightX,heightY) {
+    console.log(moonColor);
+}
