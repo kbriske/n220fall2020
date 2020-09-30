d87cbb022ad115bddf7e2aabd625360a6e9fb9eb
kbriske <70216437+kbriske@users.noreply.github.com>
2020-09-30 14:11:01 -0400

    finished personal composition

--- Assignments/Functions Exercises/Personal Composition/js/app.js
+++ Assignments/Functions Exercises/Personal Composition/js/app.js
@@ -1,17 +1,29 @@
-let moonX = 0;
-let moonY = 200;
+let moonX = 350; //moon X coordinate
+let moonY = 200; //moon Y coordinate
 
-function setup() {
+function setup() { //sets up canvas
     createCanvas(700,400);
-    background(0);
+    background(5,0,28);
 }
 
-function draw() {
-    background(0);
-    moonX++;
-    moonY++;
+function draw() { //draws the moon
+    background(5,0,28);
+    drawMoon("#e3e3e3",mouseX,mouseY);
+    stroke(5,0,28);
+    drawMoon("#05001c",mouseX-20,mouseY-10); 
+
+    if(mouseX >= 350) { //if the mouse x coordinate makes it halfway across the page, it will switch to a new background/object
+        background(143, 225, 255);
+        drawSun("#ffd452",mouseX,mouseY);
+    }
 }
 
-function drawMoon(moonColor,crater,heightX,heightY) {
-    console.log(moonColor);
+function drawMoon(moonColor,heightX,heightY) { //function for the moon
+    fill(moonColor);
+    circle(heightX, heightY,70);
+}
+
+function drawSun(sunColor, heightX, heightY) { //function for the sun
+    fill(sunColor);
+    circle(heightX, heightY, 70);
 }
