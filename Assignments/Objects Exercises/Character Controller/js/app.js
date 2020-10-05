eb2dd3f5335d40df05fc24df40df06eca54934ea
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-05 16:23:51 -0400

    started and completed character controller

---
+++ Assignments/Objects Exercises/Character Controller/js/app.js
@@ -0,0 +1,34 @@
+
+let character = {
+    x: 250,
+    y: 250,
+    r: 40,
+    color: ("#BBADFF"),
+    stroke: ("#C5D5E4"),
+    update: function() {
+        fill(this.color);
+        stroke(this.stroke);
+        circle(this.x, this.y, this.r);
+    }
+}
+
+function setup() {
+    createCanvas(500,500);
+    background("#C5D5E4");
+}
+
+function draw() {
+    background("#C5D5E4");
+    character.update();
+    if(keyCode == LEFT_ARROW) {
+        character.x -=1;
+    } else if (keyCode == RIGHT_ARROW) {
+        character.x +=1;
+    } else if (keyCode == UP_ARROW) {
+        character.y -=1;
+    } else if (keyCode == DOWN_ARROW) {
+        character.y +=1;
+    }
+}
+//"#391463" persian indigo
+//"#C5D5E4" beau blue
