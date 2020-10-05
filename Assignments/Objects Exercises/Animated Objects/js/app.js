fc490673dd7cf685b6cedf82dcbdae9ac64782b2
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-05 15:15:44 -0400

    updated some things in animated objects exercise

--- Assignments/Objects Exercises/Animated Objects/js/app.js
+++ Assignments/Objects Exercises/Animated Objects/js/app.js
@@ -1,23 +1,35 @@
-let velocity = 1;
+let velocity = 7;
 
 let dots = [
+    /*
     {x: 100, y: 200, r: 40, c: "#04E762"},
     {x: 300, y: 50, r: 40, c: "#F5B700"},
     {x: 500, y: 200, r: 40, c: "#DC0073"},
     {x: 700, y: 50, r: 40, c: "#008BF8"},
-    {x: 900, y: 200, r: 40, c: "#89FC00"}   
-]; //is the ; necessary?
+    {x: 900, y: 200, r: 40, c: "#89FC00"}  
+    */
+    {x: 100, y: 275, r: 40, c: "#04E762"},
+    {x: 300, y: 50, r: 40, c: "#F5B700"},
+    {x: 500, y: 275, r: 40, c: "#DC0073"},
+    {x: 700, y: 50, r: 40, c: "#008BF8"},
+    {x: 900, y: 275, r: 40, c: "#89FC00"}  
+];
 
 function setup() {
     createCanvas(1000,500);
-    background("#eeecda");
+    background("#F2F4FF");
 }
 
 function draw() {
-    background("#eeecda");
+    background("#F2F4FF");
     for(i = 0; i < dots.length; i++) {
         fill(dots[i].c);
         circle(dots[i].x, dots[i].y, dots[i].r);
+        dots[i].y += 10;
+        if(dots[i].y >= 480) {
+            dots[i].y = 20;
+        }
+        /*
         dots[i].y = dots[i].y + velocity;
         
 
@@ -25,6 +37,7 @@ function draw() {
             velocity = velocity * -1;
             console.log("i position: " + dots[0].x);
         }
+        */
     }
-    console.log(velocity);
 }
+
