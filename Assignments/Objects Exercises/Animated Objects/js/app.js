772ee2147cf3cb903a77ece7ac10e271561c237e
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-06 10:34:05 -0400

    changed and deleted some things on animted objects

--- Assignments/Objects Exercises/Animated Objects/js/app.js
+++ Assignments/Objects Exercises/Animated Objects/js/app.js
@@ -1,13 +1,5 @@
-let velocity = 7;
 
 let dots = [
-    /*
-    {x: 100, y: 200, r: 40, c: "#04E762"},
-    {x: 300, y: 50, r: 40, c: "#F5B700"},
-    {x: 500, y: 200, r: 40, c: "#DC0073"},
-    {x: 700, y: 50, r: 40, c: "#008BF8"},
-    {x: 900, y: 200, r: 40, c: "#89FC00"}  
-    */
     {x: 100, y: 275, r: 40, c: "#04E762"},
     {x: 300, y: 50, r: 40, c: "#F5B700"},
     {x: 500, y: 275, r: 40, c: "#DC0073"},
@@ -29,15 +21,5 @@ function draw() {
         if(dots[i].y >= 480) {
             dots[i].y = 20;
         }
-        /*
-        dots[i].y = dots[i].y + velocity;
-        
-
-        if((dots[i].y >= 480) || (dots[i].y <= 20)) {
-            velocity = velocity * -1;
-            console.log("i position: " + dots[0].x);
-        }
-        */
     }
 }
-
