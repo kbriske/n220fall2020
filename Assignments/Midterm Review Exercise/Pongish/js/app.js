5a3bcc1fc25cebbb30c05e93b97d5f1b2207b88f
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-06 20:07:47 -0400

    changed a bunch of things

--- Assignments/Midterm Review Exercise/Pongish/js/app.js
+++ Assignments/Midterm Review Exercise/Pongish/js/app.js
@@ -4,7 +4,7 @@ let velY = -3;
 
 let paddle = {
     x: 390,
-    y: 150, //why can I not make this mouseY?
+    y: 150,
     w: 10,
     h: 100,
     color: ("#20A39E"),
@@ -12,11 +12,13 @@ let paddle = {
         fill(this.color);
         stroke(this.color);
         rect(this.x, mouseY, this.w, this.h);
+        this.y = mouseY; //sets the y coordinate of paddle to the mouseY position for the rest of the object's purposes, reference as this.y
         if(this.y <= 0) {
             this.y = 0;
         } else if (this.y >= 400 - this.h) {
-            this.y = 400 - this.h; //I need this to limit mouseY's direction
+            this.y = 400 - this.h;
         }
+        rect(this.x, this.y, this.w, this.h); //adjusts the paddle to not go below the bottom boundary
     }
 }
 //uses W and S, requires change from mouseY to paddle.y on line 103
@@ -78,11 +80,9 @@ let ball = {
         circle(this.x, this.y, this.r);
         ball.x = ball.x + velX;
         ball.y = ball.y + velY;
-        if (ball.x <= 15) { //if ball hits the left side of the canvas, bounce
-            velX = velX * -1;
-        } else if ((ball.y <= 15) || (ball.y >= 385)) { //if ball hits the top or bottom of the canvas, bounce
+        if ((ball.y <= 15) || (ball.y >= 385)) { //if ball hits the top or bottom of the canvas, bounce
             velY = velY * -1;
-        } else if (ball.x >= 400) { //if ball exceeds the right side of the canvas, reset
+        } else if ((ball.x <= 0) || (ball.x >= 400)) { //if ball exceeds the right or left side of the canvas, reset
             ball.x = 200, ball.y = 90, velX = -3, velY = -3;
         }
     }
@@ -95,27 +95,19 @@ function setup() {
 
 function draw() {
     background("#FFBA49");
-    paddle.update(); //why is this green? (because of the if statements?)
+    paddle.update(); 
     paddle2.update();
     ball.update();
+
     if(hitTest(paddle.x, mouseY, paddle.w, paddle.h, ball.x + 15, ball.y + 15)) { //if it is true (implied)
-        velX = velX * -1; //how can I add some deviation (see below)
-        velY = velY * 1; //changed from -1
+        velX = velX * -1;
+        velY = random(-2,-4); //randomizes the y velocity to change things up
     }
     if(hitTest(paddle2.x, paddle2.y, paddle2.w, paddle2.h, ball.x - 15, ball.y - 15)) {
         velX = velX * -1;
-        velY = velY * 1; //changed from -1
-        /*
-        velX = velX * random(-1,-2);
-        velY = velY * random(-1,-2); 
-        */
+        velY = random(-2,-4); //randomizes the y velocity to change things up
     }
-   /*
-    if(ball.x.intersects(paddle.x)) {
-        velX = velX * -1;
-        velY = velY * -1; 
-    }
-    */
+    console.log(velY);
 }
 
 function hitTest(paddleX, paddleY, paddleW, paddleH, ballX, ballY) {
