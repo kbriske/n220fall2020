ada10a303b639ed87c9ad2938e406e5f4903c456
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-19 10:52:00 -0400

    made notes for week 9, where we started HTML

---
+++ Practice HTML/js/app.js
@@ -0,0 +1,19 @@
+let txtName = document.getElementById("txtDieSize"); //references the HTML document
+//let txtDieSize = document.querySelector("#txtDieSize"); // do we need the #?
+let dvResult = document.getElementById("dvResult");
+//dvResult.innerHTML = "Hello World";
+
+function rollDie() {
+    let dieSize = Number(txtDieSize.value);
+    let randRoll = 1 + Math.floor(Math.random() * dieSize);
+    //let dieSize = txtDieSize.value;
+    //console.log(randRoll);
+    dvResult.innerHTML = ("Roll: " + randRoll);
+
+    //txtDieSize = ""; //clears the box on refresh
+}
+
+function submit() {
+    console.log("here is your number: " + txtName.value);
+}
+
