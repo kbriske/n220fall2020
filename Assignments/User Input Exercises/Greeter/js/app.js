faa7a8da67d431151b1b4d9c115a14222710501d
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-19 16:25:54 -0400

    did a ton of work and am still with what I had

--- Assignments/User Input Exercises/Greeter/js/app.js
+++ Assignments/User Input Exercises/Greeter/js/app.js
@@ -1,9 +1,19 @@
 //REFERENCES
 let txtName = document.getElementById("txtName");
-let dvName = document.getElementById("dvName");
+let dvName = document.getElementById("dvName"); //difference?
+//let txtName = document.querySelector("#txtName");
+//let dvName = document.querySelector("#dvName");
+
+function hello(num, num2) {
+    console.log(num, num2);
+    //return (dvName.innerHTML = ("Hello " + num + "!"));
+}
+hello(3,4);
 
 function submitName() {
     let name = String(txtName.value); //initializes name as a string from the value submitted in txtName (HTML)
-    console.log("Hello " + name); //logs Hello, name to console
+    console.log("Hello, " + name);
     dvName.innerHTML = ("Hello " + name + "!");
+    //return(dvName.innerHTML = ("Hello " + name + "!"));
 }
+//submitName(txtName.value);
