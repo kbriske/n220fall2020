cbfc72e8e378d2979acc0116087c2ef9bbaa2d2c
kbriske <70216437+kbriske@users.noreply.github.com>
2020-10-27 14:59:26 -0400

    created and finished over and out, and then added it to the main index page

---
+++ Assignments/Document Object Manipulation Exercises/Over and Out/js/app.js
@@ -0,0 +1,11 @@
+//REFERENCES
+let dvSquare = document.getElementById("square");
+console.log(dvSquare);
+
+function colorSwap() {
+    dvSquare.style.backgroundColor = "#0D1317";
+}
+
+function undoColor() {
+    dvSquare.style.backgroundColor = "#232ED1";
+}
