5f993802dda6c8adee2400a5be4b36b99239baf7
kbriske <70216437+kbriske@users.noreply.github.com>
2020-11-16 09:58:06 -0500

    moved some things around, I will come back for you (project that uses arrays instead of loops)

--- Assignments/Array Computation Exercises/Bad Word Catcher (Array Method)/js/app.js
+++ Assignments/Array Computation Exercises/Bad Word Catcher (Array Method)/js/app.js
@@ -10,17 +10,24 @@ badWordTotal = 0;
 function filterText() {
     var splitString = txtInput.value.split(" "); //splits the input by spaces
     
-    allWords.push(splitString); //adds the input to an array
-    console.log(allWords);
+    //allWords.push(txtInput.value); //adds the input to an array
+    //console.log(allWords);
 
+    var found = splitString.find(element => element == "clear")
+    badWordTotal += 1;
+    console.log(badWordTotal);
+    var found1 = splitString.find(element => element == "water")
+    badWordTotal += 1;
+    var found2 = splitString.find(element => element == "tires")
+    console.log(found);
+    dvWordCount.innerHTML = found;
     output.innerHTML = ""; //sets the output back to nothing
    
-
     for(i = 0; i < allWords.length; i++) { //loops through all the words in the input
 
         var newEl = document.createElement("div"); //creates a new div
         newEl.innerHTML = allWords[i]; //displays the array of words in the new div
-        
+        /*
         if(allWords[i] == "clear") {
             badWordTotal += 1;
         } else if (allWords[i] == "water") {
@@ -28,6 +35,7 @@ function filterText() {
         } else if (allWords[i] == "tires") {
             badWordTotal += 1;
         }
+        */
         output.appendChild(newEl);
         dvBadWordCount.innerHTML = "bad words: " + badWordTotal;
         console.log("bad words: " + badWordTotal);
