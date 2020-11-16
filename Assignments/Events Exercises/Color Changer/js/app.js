2fdca28293da983e0324ed97375021f5baa5ad45
kbriske <70216437+kbriske@users.noreply.github.com>
2020-11-16 17:52:57 -0500

    added some stuff and figured things out thanks to Travis Faas, exercise one = complete

--- Assignments/Events Exercises/Color Changer/js/app.js
+++ Assignments/Events Exercises/Color Changer/js/app.js
@@ -3,7 +3,8 @@ let tile = document.getElementById("dvTile1");
 let tile2 = document.getElementById("dvTile2");
 let tile3 = document.getElementById("dvTile3");
 
-/*
+colors = ["#ff4542","#5df067","#3680f7"]
+
 for(i = 0; i < 3; i++) {
     let newTile = document.createElement("div");
     newTile.style.width = "200px";
@@ -18,45 +19,17 @@ for(i = 0; i < 3; i++) {
     tile.appendChild(newTile);
 
     newTile.addEventListener("click", msClicked);
+    newTile.setAttribute("data-color",colors[i]);
 }
-*/
 
 tile.addEventListener("click", msClicked);
 tile2.addEventListener("click", msClicked);
 tile3.addEventListener("click", msClicked);
 
-tile.style.width = "200px";
-tile.style.height = "200px";
-tile.style.backgroundColor = "#222";
-tile.style.cssFloat = "left";
-tile.style.margin = "5px";
-
-tile2.style.width = "200px";
-tile2.style.height = "200px";
-tile2.style.backgroundColor = "#222";
-tile2.style.cssFloat = "left";
-tile2.style.margin = "5px";
-
-tile3.style.width = "200px";
-tile3.style.height = "200px";
-tile3.style.backgroundColor = "#222";
-tile3.style.cssFloat = "left";
-tile3.style.margin = "5px";
-
 function msClicked(event) {
 
-    let code = Number(event.target.getAttribute("data-code"));
+    let color = event.target.getAttribute("data-color");
     //console.log(event.target);
-    event.target.innerHTML = code;
-
-    if(code == 1) {
-        event.target.style.backgroundColor = "#ff4542";
-    }
-    else if(code == 2) {
-        event.target.style.backgroundColor = "#5df067";
-    }
-    else if(code == 3) {
-        event.target.style.backgroundColor = "#3680f7";
-    }
+    event.target.style.backgroundColor = color;
 }
 
