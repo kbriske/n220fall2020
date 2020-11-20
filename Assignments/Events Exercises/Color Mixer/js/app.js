e906bb19681db9f4530c39e3376de21e559e3508
kbriske <70216437+kbriske@users.noreply.github.com>
2020-11-19 21:28:52 -0500

    tried to get a start on the colormixer exercise

--- Assignments/Events Exercises/Color Mixer/js/app.js
+++ Assignments/Events Exercises/Color Mixer/js/app.js
@@ -1,1 +1,13 @@
 //REFERENCES
+canvas = document.getElementById("dvCanvas");
+currentColor = document.getElementById("dvCurrentColor");
+addRed = document.getElementById("dvRed");
+addRed5 = document.getElementById("dvRed5");
+
+currentColor.addEventListener("click", msClicked);
+addRed = addEventListener("click", msClicked);
+
+function msClicked(event) {
+    console.log(event.target);
+}
+
