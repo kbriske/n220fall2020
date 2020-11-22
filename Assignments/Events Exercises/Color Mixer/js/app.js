aed62041453fb793246737f647fbf228c359fd28
kbriske <70216437+kbriske@users.noreply.github.com>
2020-11-22 10:01:16 -0500

    fixed and completed color mixer

--- Assignments/Events Exercises/Color Mixer/js/app.js
+++ Assignments/Events Exercises/Color Mixer/js/app.js
@@ -1,13 +1,46 @@
 //REFERENCES
 canvas = document.getElementById("dvCanvas");
 currentColor = document.getElementById("dvCurrentColor");
-addRed = document.getElementById("dvRed");
-addRed5 = document.getElementById("dvRed5");
 
-currentColor.addEventListener("click", msClicked);
-addRed = addEventListener("click", msClicked);
+//variables
+let red = 0;
+let green = 0;
+let blue = 0;
 
-function msClicked(event) {
-    console.log(event.target);
+function addColor(event) {
+    var getColor = event.target.getAttribute("data-color");
+    var getValue = event.target.getAttribute("data-value");
+    
+    if(getColor == "red") {
+        if(getValue == 1){
+            red++;
+        } else if(getValue == 5) {
+            red+= 5;
+        } else if(getValue == 10) {
+            red += 10;
+        }
+    }
+    else if(getColor == "green") {
+        if(getValue == 1){
+            green++;
+        } else if(getValue == 5) {
+            green += 5;
+        } else if(getValue == 10) {
+            green += 10;
+        }
+    }
+    else if(getColor == "blue") {
+        if(getValue == 1){
+            blue++;
+        } else if(getValue == 5) {
+            blue+= 5;
+        } else if(getValue == 10) {
+            blue+= 10;
+        }
+    }
+    
+    finalColor = "rgb(" + red +", " + green +", " + blue +")"
+    currentColor.innerHTML = "Current Color: " + finalColor;
+    canvas.style.backgroundColor = finalColor;
 }
 
