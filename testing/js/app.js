203cf5b5eca3e0eb860e9f63b15cb107bf660749
kbriske <70216437+kbriske@users.noreply.github.com>
2020-12-03 15:06:28 -0500

    messed around with some code

--- testing/js/app.js
+++ testing/js/app.js
@@ -6,3 +6,25 @@ function createElement() {
     space.appendChild(blank);
 }
 }
+
+var v = 1; 
+
+var f1 = function () { 
+  console.log(v); 
+} 
+
+var f2 = function() { 
+  var v = 2; 
+  f1(); 
+}; 
+
+f2();
+let modal = document.querySelector('#results');
+setTimeout(function() {
+ modal.classList.remove('hidden');
+}, 10000);
+console.log('Results shown');
+
+const dessert = { type: 'pie' }; 
+dessert.type = 'pudding';
+console.log(dessert.type);
