35658d2cf708d7e0b5e0d5c8320ea88797464970
kbriske <70216437+kbriske@users.noreply.github.com>
2020-12-07 12:42:08 -0500

    messed around I guess

--- Assignments/Final Project (Concentration)/js/app.js
+++
@@ -1,9 +0,0 @@
-//REFERENCES
-card = document.getElementById("dvCard");
-
-for(i = 0; i < 16; i ++) {
-let newEl = document.createElement("div");
-    dvCard.appendChild(newEl);
-    console.log(newEl);
-}
-
