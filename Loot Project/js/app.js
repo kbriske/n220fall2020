5e52691b681b90cac372f07ea57a93f002cf17c3
kbriske <70216437+kbriske@users.noreply.github.com>
2020-12-28 12:00:24 -0500

    just messing around with a personal project

---
+++ Loot Project/js/app.js
@@ -0,0 +1,7 @@
+//REFERENCES
+
+let numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+for(i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
