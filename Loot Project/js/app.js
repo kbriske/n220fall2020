e5dd4b595755950b21535fc89aac8db64aff002e
kbriske <70216437+kbriske@users.noreply.github.com>
2021-01-03 11:44:38 -0500

    deleted project

--- Loot Project/js/app.js
+++
@@ -1,7 +0,0 @@
-//REFERENCES
-
-let numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-
-for(i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
